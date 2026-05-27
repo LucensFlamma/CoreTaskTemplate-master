e834631c87559af83b9a7783227cc8a373ec943d
Svetlana Mozhaeva <svetlana.mozhaeva@hotmail.com>
2026-05-27 11:45:38 +0300

    Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -12,31 +20,162 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                "id BIGSERIAL PRIMARY KEY, " +
+                "name VARCHAR(255), " +
+                "lastName VARCHAR(255), " +
+                "age SMALLINT)";
 
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = Util.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+
+            System.out.println("Successfully created users table via Hibernate");
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users";
 
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = Util.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+
+            System.out.println("Successfully deleted users table via Hibernate");
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Session session = null;
+        Transaction transaction = null;
 
+        try {
+            session = Util.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+
+            transaction.commit();
+
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = Util.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            User user = (User) session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+
+           transaction.commit();
+
+            System.out.println("User с id – " + id + " удалён из базы данных ");
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
 
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+
+        Session session = null;
+
+        try {
+            session = Util.getSessionFactory().openSession();
+            users = session.createQuery("FROM User").list();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
+        String sql = "TRUNCATE TABLE users";
+
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = Util.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
     private static final String URL = "jdbc:postgresql://localhost:5432/1_1_3_db";
     private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "my password";
+    private static final String PASSWORD = "383838";
+
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         Connection connection = null;
@@ -19,6 +27,40 @@ public class Util {
             System.out.println("Ошибка соединения с БД!");
         }
         return connection;
+    }
+
+    // SessionFactory Hibernate
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                // Настройки Hibernate без hibernate.cfg.xml
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USERNAME);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+
+                // SQL-запросы в консоли
+                settings.put(Environment.SHOW_SQL, "true");
+
+                // Управление контекстом сессий (чтобы сессия привязывалась к текущему потоку)
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                configuration.setProperties(settings);
+
+                // Регистрация класса User
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (Exception e) {
+                System.out.println("Ошибка при создании SessionFactory!");
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 
 }
